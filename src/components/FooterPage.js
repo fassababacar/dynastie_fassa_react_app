8bf38bedaa81b3fe39786a6bf9d1b97ce29c6322
Babacar <babacar.fassa@ambitiontelecom.com>
2022-06-09 16:42:08 +0200

    IMP: Changement url dans le footer

--- src/components/FooterPage.js
+++ src/components/FooterPage.js
@@ -5,7 +5,7 @@ export class FooterPage extends React.Component {
         return (
             <footer className="footer py-2 bg-dark fixed-bottom text-center text-light">
                 <span >Company © All rights reserved by </span>
-                <a href="https://fassababacar.github.io/dynastie_fassa/">Babacar FASSA</a>
+                <a href="https://fassababacar.github.io/dynastie_fassa_react_app/">Babacar FASSA</a>
             </footer>
         );
     }
